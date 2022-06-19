27e128587c3c6a16546df8b98fbc9fa95401bc01
Karlen777 <karltigranovich@gmail.com>
2022-06-19 23:15:12 +0400

    first commit

---
+++ apploop.js
@@ -0,0 +1,24 @@
+// while num
+let i = 0;
+while (i < 3) {
+    console.log ( i );
+    i++;
+}
+let a = 3;
+while (a) {
+    console.log(a);
+    a--;
+}
+let b = 0;
+do {
+    console.log( b );
+    b++;
+} while (b < 3);
+
+let x = 0;
+for (; x < 3;) {
+    console.log( x++);
+}
+for (let y = 0; y < 5; y++) {
+    console.log( `tiv ${y}`);
+}
